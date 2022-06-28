e3a3f13c8eee99863d04f8cafed9aa5ee6a59602
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-06-28 20:41:05 -0300

    Validando dados

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -17,7 +17,15 @@ module.exports = (sequelize, DataTypes) => {
   Pessoas.init({
     nome: DataTypes.STRING,
     ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Dado do tipo e-mail inválido!'
+        }
+      }
+    },
     role: DataTypes.STRING
   }, {
     sequelize,
