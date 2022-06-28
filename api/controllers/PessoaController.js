6e34fb996f8a1aa8fe9af4a550bdf9bc1e38826e
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-06-27 23:34:38 -0300

    Métodos de CRUD

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -10,6 +10,22 @@ class PessoaController {
             return res.status(500).json(error.message);
         }
     }
+
+    static async pegaUmaPessoa(req, res) {
+        const { id } = req.params;
+
+        try {
+            const umaPessoa = await database.Pessoas.findOne({
+                where: { 
+                    id: Number(id)
+                }
+            });
+
+            return res.status(200).json(umaPessoa);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
 }
 
 module.exports = PessoaController;
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,5 +4,6 @@ const PessoaController = require('../controllers/PessoaController');
 const router = Router();
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas);
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
 
 module.exports = router;
