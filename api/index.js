2e07f0e384ec52ac0d74186ab5c7a82145035225
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-06-27 23:19:43 -0300

    Organizando as rotas

--- api/index.js
+++ api/index.js
@@ -1,17 +1,11 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const routes = require('./routes');
 
-const app = express()
+const app = express();
+const port = 3000;
 
-app.use(bodyParser.json())
+routes(app);
 
-const port = 3000
-
-app.get('/teste', (req, res) => res
-    .status(200)
-    .send({ mensagem: 'Boas-vindas à API'})
-)
-
-app.listen(port, () => console.log(`Servidor está rodando na porta ${port}`))
+app.listen(port, () => console.log(`Servidor está rodando na porta ${port}`));
 
 module.exports = app
