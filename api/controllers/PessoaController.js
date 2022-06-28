bb2a1462b3ff2fb5d9decce99fc5c7432446a5a6
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-06-27 23:54:40 -0300

    CRUD: Atualizar e deletar

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -38,6 +38,45 @@ class PessoaController {
             return res.status(500).json(error.message);
         }
     }
+
+    static async atualizaPessoa(req, res) {
+        const { id } = req.params;
+        const novasInfos = req.body;
+
+        try {
+            await database.Pessoas.update(novasInfos, {
+                where: {
+                    id: Number(id)
+                }
+            });
+            
+            const pessoaAtualizada = await database.Pessoas.findOne({
+                where: { 
+                    id: Number(id)
+                }
+            });
+
+            return res.status(200).json(pessoaAtualizada);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async apagaPessoa(req, res) {
+        const { id } = req.params;
+
+        try {
+            await database.Pessoas.destroy({
+                where: { 
+                    id: Number(id)
+                }
+            });
+            
+            return res.status(200).json({ mensagem: `id ${id} deletado!` });
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
 }
 
 module.exports = PessoaController;
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -6,5 +6,7 @@ const router = Router();
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas);
 router.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
 router.post('/pessoas', PessoaController.criaPessoa);
+router.put('/pessoas/:id', PessoaController.atualizaPessoa);
+router.delete('/pessoas/:id', PessoaController.apagaPessoa);
 
 module.exports = router;
