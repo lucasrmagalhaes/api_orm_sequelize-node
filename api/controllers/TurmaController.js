e528ec4574394b3d60743ce69149c379cb97155c
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-06-28 21:32:19 -0300

    Usando operadores

--- api/controllers/TurmaController.js
+++ api/controllers/TurmaController.js
@@ -1,9 +1,25 @@
 const database = require('../models');
+const Sequelize = require('sequelize');
+
+const Op = Sequelize.Op;
 
 class TurmaController {
     static async pegaTodasAsTurmas(req, res) {
+        const {
+            data_inicial,
+            data_final
+        } = req.query;
+        
+        const where = {};
+
+        data_inicial || data_final ? where.data_inicio = {} : null;
+        data_inicial ? where.data_inicio[Op.gte] = data_inicial : null;
+        data_final ? where.data_inicio[Op.lte] = data_final : null;
+
         try {
-            const todasAsTurmas = await database.Turmas.findAll()
+            const todasAsTurmas = await database.Turmas.findAll({
+                where
+            })
         
             return res.status(200).json(todasAsTurmas)
         } catch (error) {
