975f5c461b4688e7bb17fc346c6e20b4dd50f6bf
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-06-28 21:52:27 -0300

    Outros agregadores

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,4 +1,5 @@
 const database = require('../models');
+const Sequelize = require('sequelize');
 
 class PessoaController {
     static async pegaPessoasAtivas(req, res) {
@@ -215,9 +216,26 @@ class PessoaController {
                 order: [[ 'estudante_id', 'ASC' ]]
             });
 
-            // return res.status(200).json(todasAsMatriculas.count);
-
             return res.status(200).json(todasAsMatriculas.count);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async pegaTurmasLotadas(req, res) {
+        const lotacaoTurma = 1;
+
+        try {
+            const turmasLotadas = await database.Matriculas.findAndCountAll({
+                where: {
+                    status: 'confirmado'
+                },
+                attributes: ['turma_id'],
+                group: ['turma_id'],
+                having: Sequelize.literal(`count(turma_id) >= ${lotacaoTurma}`)
+            })
+
+            return res.status(200).json(turmasLotadas.count);
         } catch (error) {
             return res.status(500).json(error.message);
         }
